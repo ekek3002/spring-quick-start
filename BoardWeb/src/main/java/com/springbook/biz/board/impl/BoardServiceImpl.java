7143172c691ce3e4623eac1b2e0750318469d37d
이형균 <ekek3002@gmail.com>
2022-01-13 00:50:07 +0900

    DAY2 트랜잭션 처리

--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -11,10 +11,15 @@ import java.util.List;
 public class BoardServiceImpl implements BoardService {
 
     @Autowired
-    private BoardDAO boardDAO;
+    private BoardDAOSpring boardDAO;
+//    private BoardDAO boardDAO;
 
     @Override
     public void insertBoard(BoardVO vo) {
+//        if (vo.getSeq() == 0) {
+//            throw new IllegalArgumentException("0번 글은 등록할 수 없습니다.");
+//        }
+//        boardDAO.insertBoard(vo);
         boardDAO.insertBoard(vo);
     }
 
